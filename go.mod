6af1c03d2e0c250d3f7f7286bb9827012656b8bf
DosGo <dosgo@qq.com>
2022-03-04 00:00:42 +0800

    test...

---
+++ go.mod
@@ -0,0 +1,18 @@
+module easy-wireguard
+
+go 1.17
+
+require golang.zx2c4.com/wireguard/wgctrl v0.0.0-20220208144051-fde48d68ee68
+
+require (
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/mdlayher/genetlink v1.2.0 // indirect
+	github.com/mdlayher/netlink v1.6.0 // indirect
+	github.com/mdlayher/socket v0.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20220208050332-20e1d8d225ab // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+	golang.zx2c4.com/wireguard v0.0.0-20220202223031-3b95c81cc178 // indirect
+)
---
+++ tool/tool.go
@@ -0,0 +1,38 @@
+package tool
+import(	"net"
+"log"
+"encoding/hex"
+"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+// MustHexKey decodes a hex string s as a key or panics.
+func MustHexKey(s string) wgtypes.Key {
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		log.Printf("wgtest: failed to decode hex key: %v", err)
+	}
+
+	k, err := wgtypes.NewKey(b)
+	if err != nil {
+		log.Printf("wgtest: failed to create key: %v", err)
+	}
+
+	return k
+}
+
+func MustUDPAddr(s string) *net.UDPAddr {
+	a, err := net.ResolveUDPAddr("udp", s)
+	if err != nil {
+		log.Printf("wgtest: failed to resolve UDP address: %v", err)
+	}
+
+	return a
+}
+
+func MustCIDR(s string) net.IPNet {
+	_, cidr, err := net.ParseCIDR(s)
+	if err != nil {
+		log.Printf("wgtest: failed to parse CIDR: %v", err)
+	}
+
+	return *cidr
+}
