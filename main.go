3eb506913bbf33ca6441171b2e23cc2c8cd2928c
dosgo <dosgo@qq.com>
2022-09-29 18:57:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"easy-wireguard/tool"
+	"encoding/hex"
 	"fmt"
 	"net"
+	"os"
+	"path/filepath"
+	"strconv"
 	"time"
 
 	"golang.zx2c4.com/wireguard/wgctrl"
@@ -12,27 +16,108 @@ import (
 
 func main() {
 	var name = "wg0"
+	fName := filepath.Base(os.Args[0])
+	if len(os.Args) < 2 {
+		help(fName)
+		return
+	}
+	if len(os.Args) >= 3 {
+		name = os.Args[2]
+	}
+	/*启动*/
+	if os.Args[1] == "start" {
+		start(name)
+	} else if os.Args[1] == "genServerConf" {
+		//生成配置文件
+		genServerConf(name)
+	} else if os.Args[1] == "addPeer" {
+		//添加addPeer节点
+		addPeer(name)
+	} else {
+		help(fName)
+	}
+}
+
+func help(fName string) {
+	fmt.Println(fName + " start  [name]")
+	fmt.Println(fName + " genServerConf  [name]")
+	fmt.Println(fName + " addPeer  [name]")
+}
+
+func start(name string) {
+	_, err := os.Stat(name + ".conf")
+	if err != nil {
+		genServerConf(name)
+	}
+	conf := tool.FileToConf(name)
 	client, _ := wgctrl.New()
 	defer client.Close()
+	go tool.WgUp(name)
+	time.Sleep(time.Second * 35)
+	client.ConfigureDevice(name, conf)
+}
+func genServerConf(name string) {
+	_, err := os.Stat(name + ".conf")
+	if err == nil {
+		fmt.Println("Configuration file " + name + ".conf already exists")
+		return
+	}
 	serverKey, _ := wgtypes.GeneratePrivateKey()
-	peers, _ := wgtypes.GeneratePrivateKey()
 	var conf = wgtypes.Config{
 		PrivateKey:   tool.KeyPtr(serverKey),
-		ListenPort:   tool.IntPtr(12912),
-		FirewallMark: tool.IntPtr(0),
+		ListenPort:   tool.IntPtr(51820),
 		ReplacePeers: true,
+	}
+	tool.ConfToFile(name, conf)
+}
+func addPeer(name string) {
+	peerKey, _ := wgtypes.GeneratePrivateKey()
+	//读取服务器配置文件
+	ServerConf := tool.FileToConf(name)
+	//生成peer配置文件
+	var allowedIPs = []net.IPNet{
+		tool.MustCIDR("192.168.6.1/32"),
+	}
+	ip, _ := tool.GetPublicIP()
+	var endpoint = tool.MustUDPAddr(ip + ":" + strconv.Itoa(*ServerConf.ListenPort))
+	var peerConf = wgtypes.Config{
+		PrivateKey: tool.KeyPtr(peerKey),
 		Peers: []wgtypes.PeerConfig{
 			{
-				PublicKey: peers.PublicKey(),
-				Endpoint:  tool.MustUDPAddr("[abcd:23::33%2]:51820"),
-				AllowedIPs: []net.IPNet{
-					tool.MustCIDR("192.168.4.4/32"),
-				},
+				PublicKey:  ServerConf.PrivateKey.PublicKey(),
+				Endpoint:   endpoint,
+				AllowedIPs: allowedIPs,
 			},
 		},
 	}
-	go tool.WgUp(name)
-	time.Sleep(time.Second * 35)
-	err := client.ConfigureDevice(name, conf)
-	fmt.Printf("err:%v\r\n", err)
+	//添加到服务器peer节点
+	peerInfo := wgtypes.PeerConfig{
+		PublicKey:  peerKey.PublicKey(),
+		AllowedIPs: allowedIPs,
+	}
+	ServerConf.Peers = append(ServerConf.Peers, peerInfo)
+	//生成server配置文件
+	tool.ConfToFile(name, ServerConf)
+	//生成peer配置文件
+	pubKeyByte := peerKey.PublicKey()
+	tool.ConfToFile(name+"_peer_"+hex.EncodeToString(pubKeyByte[:]), peerConf)
+}
+
+func delPeer(name string, pubKey string) {
+	peerKey, _ := wgtypes.GeneratePrivateKey()
+	//读取服务器配置文件
+	ServerConf := tool.FileToConf(name)
+	publicKey, _ := wgtypes.ParseKey(pubKey)
+	for _, peer := range ServerConf.Peers {
+		if peer.PublicKey == publicKey {
+
+		}
+	}
+
+	ServerConf.Peers = append(ServerConf.Peers, peerInfo)
+	//生成server配置文件
+	tool.ConfToFile(name, ServerConf)
+	//生成peer配置文件
+	pubKeyByte := peerKey.PublicKey()
+	tool.ConfToFile(name+"_peer_"+hex.EncodeToString(pubKeyByte[:]), peerConf)
 }
